444cbebd44b0d3bc2af25afb3e5d448b8160eecd
AlibekDalgat <johnbuck_kebarty@mail.ru>
2025-07-18 13:49:36 +0300

    delivery layer

---
+++ internal/delivery/handler.go
@@ -0,0 +1,33 @@
+package delivery
+
+import (
+	"github.com/gin-gonic/gin"
+	"marketplace/internal/service"
+)
+
+type Handler struct {
+	services *service.Service
+}
+
+func NewHandler(s *service.Service) *Handler {
+	return &Handler{services: s}
+}
+
+func (h *Handler) InitRoutes() *gin.Engine {
+	router := gin.New()
+
+	auth := router.Group("/auth")
+	{
+		auth.POST("/sign-up", h.signUp)
+		auth.POST("/sign-in", h.signIn)
+	}
+	api := router.Group("/api", h.userIdentity)
+	{
+		adverts := api.Group("/adverts")
+		{
+			adverts.POST("/", h.createAdvert)
+			adverts.GET("/", h.listAdverts)
+		}
+	}
+	return router
+}
